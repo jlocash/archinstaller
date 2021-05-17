fa5b31826f02fd24f2eda4a741eb971d6d0d10d1
Joshua Locash <locashjosh@gmail.com>
2021-05-16 23:28:51 -0400

    added chroot step
    Signed-off-by: Joshua Locash <locashjosh@gmail.com>

---
+++ scripts/4-exec_chroot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+source "$(dirname $0)/utils.sh"
+
+log "Copying chroot scripts to /mnt"
+cp $(dirname $0)/{chroot,utils}.sh /mnt
+log "Executing chroot"
+arch-chroot /mnt ./chroot.sh
+
--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/env bash
 set -e
 source utils.sh
-source install.conf
-source chroot.conf
 
-# configure timezone
-log_info "Configuring timezone, date and locale"
-timedatectl set-ntp true
-timedatectl set-timezone "$TARGET_TIMEZONE"
+CRYPT_PATH="$(findmnt -n -o SOURCE --target=/ | cut -d '[' -f 1)"
+CRYPT_NAME="$(echo $CRYPT_PATH | cut -d '/' -f 4)"
+ROOT_PART="$(cryptsetup status $CRYPT_NAME | grep device: | cut -d ':' -f 2 | xargs)"
+GRUB_CRYPT_ENTRY="rd.luks.name=$(blkid -s UUID -o value $ROOT_PART)=$CRYPT_NAME root=/dev/mapper/$CRYPT_NAME"
 
-# apply locale
+hwclock --systohc
+
+# locale
+log "Configuring system locale"
+TARGET_LOCALE="en_US.UTF-8"
+read -p "enter desired locale [en_US.UTF-8]: " TARGET_LOCALE
 echo "$TARGET_LOCALE $(echo $TARGET_LOCALE | cut -d '.' -f 2)" >>/etc/locale.gen
 locale-gen
 sed -i "s/__TARGET_LOCALE__/$TARGET_LOCALE/" /etc/locale.conf
 
-# apply keymap
+# keymap
+log "Configuring system keymap"
+TARGET_KEYMAP="us"
+read -p "enter desired keymap [us]: " TARGET_KEYMAP
 sed -i "s/__TARGET_KEYMAP__/$TARGET_KEYMAP/" /etc/vconsole.conf
 
-# configure hostname
-log_info "Configuring system hostname"
+# hostname
+log "Configuring system hostname"
+TARGET_HOSTNAME="archlinux"
+read -p "enter desired hostname [archlinux]: " TARGET_HOSTNAME
 echo "$TARGET_HOSTNAME" >/etc/hostname
 sed -i "s/__TARGET_HOSTNAME__/$TARGET_HOSTNAME/g" /etc/hosts
 
 # enable systemd services
+log "Enabling systemd services"
 systemctl enable \
   NetworkManager \
   fstrim.timer \
@@ -35,21 +44,19 @@ systemctl enable \
   gdm
 
 # install and configure GRUB
-log_info "Installing the GRUB bootloader"
+log "Installing the GRUB bootloader"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=archlinux
-GRUB_CRYPT_ENTRY="rd.luks.name=$(blkid -s UUID -o value $TARGET_ROOT_PARTITION)=$TARGET_CRYPT_NAME root=/dev/mapper/$TARGET_CRYPT_NAME"
 sed -i "s|__GRUB_CRYPT_ENTRY__|$GRUB_CRYPT_ENTRY|" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Regenerate initramfs
+log "Regenerating initramfs"
 mkinitcpio -p linux
 
 # create administrator user
-log_info "Creating local administrator account"
-read -p "Enter username: " username
-
+log "Creating local administrator account"
+username=""
 while [[ "$username" == "" ]]; do
-  log_info "Invalid username: \"$username\""
   read -p "Enter username: " username
 done
 
@@ -60,5 +67,5 @@ passwd "$username"
 sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 
 # Lock root account
-log_info "Locking root account"
+log "Locking root account"
 passwd -l root
