bf7d522c0afd60ac66f1f5efc3ae32044b02b572
Joshua Locash <locashjosh@gmail.com>
2021-05-19 21:53:40 -0400

    fixes
    Signed-off-by: Joshua Locash <locashjosh@gmail.com>

--- 2-select-packages.sh
+++ 2-select-packages.sh
@@ -21,15 +21,13 @@ pkglist=(
 
 log "Selecting packages for system installation"
 
-firewalld_enable="Y"
 read -p "Add firewalld? [Y/n]: " firewalld_enable
-if [[ $firewalld_enable =~ ^[Yy]$ ]]; then
+if [[ ! $firewalld_enable =~ ^[Nn]$ ]]; then
   pkglist+=("firewalld")
 fi
 
-virt_enable="Y"
 read -p "Add virtualization tools? [Y/n]: " virt_enable
-if [[ $virt_enable =~ ^[Yy]$ ]]; then
+if [[ ! $virt_enable =~ ^[Nn]$ ]]; then
   pkglist+=(
     "libvirt"
     "edk2-ovmf"
@@ -42,9 +40,8 @@ if [[ $virt_enable =~ ^[Yy]$ ]]; then
   )
 fi
 
-gnome_enable="Y"
 read -p "Add the GNOME desktop? [Y/n]: " gnome_enable
-if [[ $gnome_enable =~ ^[Yy]$ ]]; then
+if [[ ! $gnome_enable =~ ^[Nn]$ ]]; then
   pkglist+=(
     "gnome"
     "alsa-utils"
@@ -53,9 +50,8 @@ if [[ $gnome_enable =~ ^[Yy]$ ]]; then
     "firefox"
   )
 
-  pipewire_enable="Y"
-  read -p "Replace pulseaudio with PipeWire? [Y/n]: " pipewire_enable
-  if [[ $pipewire_enable =~ ^[Yy]$ ]]; then
+  read -p "Replace PulseAudio with PipeWire? [Y/n]: " pipewire_enable
+  if [[ ! $pipewire_enable =~ ^[Nn]$ ]]; then
     pkglist+=(
       "pipewire"
       "pipewire-alsa"
@@ -64,9 +60,8 @@ if [[ $gnome_enable =~ ^[Yy]$ ]]; then
     )
   fi
 
-  flatpak_enable="Y"
   read -p "Add Flatpak? [Y/n]: " flatpak_enable
-  if [[ $flatpak_enable =~ ^[Yy]$ ]]; then
+  if [[ ! $flatpak_enable =~ ^[Nn]$ ]]; then
     pkglist+=(
       "pipewire"
       "pipewire-alsa"
